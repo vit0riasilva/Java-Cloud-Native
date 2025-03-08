e51a6af8b2e9f7858d8a820f1cf34174c673dec9
Vitória Silva <silva.vitoria18@outlook.com>
2025-03-07 21:38:25 -0400

    a4 - Aprendendo sobre o objeto Scannner

---
+++ java-basic/src/Scan.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Scan {
+    
+    public static void main(String[] args) {
+        //criação do objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("-------------------------------");
+        System.out.println("Dados do Usuario");
+        System.out.println("Nome Completo: " + nome + " " + sobrenome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Altura: " + altura + "cm");
+        
+    }
+    
+}
