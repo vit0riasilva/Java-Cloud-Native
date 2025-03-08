dc48a81dd59bac5be0a7b12b858548027bd1041d
Vitória Silva <silva.vitoria18@outlook.com>
2025-03-07 20:59:44 -0400

    a3 - documentação java

---
+++ java-basic/src/Doc.java
@@ -0,0 +1,23 @@
+
+/**
+ * <h1>Documentação</h1>
+ * A Documentação é um documento importante que contém informações importantes sobre a classe.
+ * <p>
+ * <b>Note:</b> Leia com atenção a documentação desta classe para desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @author Vitória Silva
+ * @version 1.0
+ * @since 07/03/2025
+ */
+public class Doc {
+    /**
+     * Este método só serve de exemplo sobre como fazer uma documentação em Java
+     * @param teste aqui é especificado sobre o parâmetro utilizado no método
+     * @return aqui é especificado sobre o retorno do método
+     */
+
+    public int testar(int teste) {
+        teste++;
+        return teste;
+    }
+}
