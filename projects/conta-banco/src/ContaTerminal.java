8672770ac76314f139c340322233ceea669721be
Vitória Silva <silva.vitoria18@outlook.com>
2025-03-10 14:25:20 -0400

    p2 - desafio conta bancária

--- projects/conta-banco/src/ContaTerminal.java
+++ projects/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * <h1>Conta Terminal</h1>
+ * <p> Simulando a criação de uma uma conta bancária com Java </p>
+ * 
+ * @author Vitória Silva
+ * @version 1.0
+ * @since 10/03/2025
+ */
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Bem-vindo ao Banco XXX!");
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+        System.out.println("Por favor, digite o número da Conta: ");
+        int numeroConta = scanner.nextInt();
+        System.out.println("Por favor, digite o número da Agência com digito: ");
+        String agencia = scanner.next();
+        System.out.println("E para finalizar, digite seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigada por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numeroConta + " e seu saldo R$" + saldo + " já está disponível para saque.");
+        
     }
 }
