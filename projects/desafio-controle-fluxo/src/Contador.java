a0f1205ebfb27b4e850f33fafc9d9baf9bce888d
Vitória Silva <silva.vitoria18@outlook.com>
2025-03-13 12:27:09 -0400

    p4 - desafio contador

---
+++ projects/desafio-controle-fluxo/src/Contador.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+
+/**
+ * <h1> Desafio Controle de Fluxo </h1>
+ * <p> O sistema deverá receber dois parâmetros via terminal que representarão dois números inteiros, com estes dois números você deverá obter a quantidade de interações (for) e realizar a impressão no console (System.out.print) dos números incrementados </p>
+ * 
+ * @author Vitória Silva
+ * @since 12/03/2025
+ * @version 1.0
+ */
+
+public class Contador {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+        }
+        terminal.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        } else {
+            int contagem = parametroDois - parametroUm;
+            for (int i = 1; i <= contagem; i++) {
+                System.out.println("Imprimindo o número " + i);
+            }
+        }
+    }
+
+}
