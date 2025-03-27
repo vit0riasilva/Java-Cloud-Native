d49cbc9b619af113249c84b1cf1f969f8c7cbf78
Vitória Silva <silva.vitoria18@outlook.com>
2025-03-27 12:56:38 -0400

    a1 - contexto inicial

---
+++ java-poo/MSN-mensseger/src/MSNMensseger.java
@@ -0,0 +1,18 @@
+public class MSNMensseger {
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem");
+    }
+
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+
+    public void validarConectadoInternet() {
+        System.out.println("Validando se está conectado a internet");
+    }
+
+    public void salvarHistoricoMensagem() {
+        System.out.println("Salvando o histórico das mensagens");
+    }
+
+}
