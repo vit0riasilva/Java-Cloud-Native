626d204d6c99ca94aa0398ad535493e89638a643
Vitória Silva <silva.vitoria18@outlook.com>
2025-03-27 13:14:33 -0400

    a3 - herança

--- java-poo/MSN-mensseger/src/Computador.java
+++ java-poo/MSN-mensseger/src/Computador.java
@@ -1,7 +1,17 @@
 public class Computador {
     public static void main(String[] args) {
         MSNMessenger msn = new MSNMessenger();
+        FacebookMessenger face = new FacebookMessenger();
+        Telegram tlg = new Telegram();
 
+        System.out.println("MSN");
         msn.enviarMensagem();
+
+        System.out.println("Facebook");
+        face.enviarMensagem();
+
+        System.out.println("Telegram");
+        tlg.enviarMensagem();
+
     }
 }
