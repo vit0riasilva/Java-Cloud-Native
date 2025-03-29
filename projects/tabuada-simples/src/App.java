49a936c5b8eb8f69a58625e9f468a445d74e0315
Vitória Silva <silva.vitoria18@outlook.com>
2025-03-28 21:14:06 -0400

    p5 - gerador de tabuada

--- projects/tabuada-simples/src/App.java
+++ projects/tabuada-simples/src/App.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * Escreva um código onde o usuário entra com um número e seja gerada a tabuada de 1 até 10 desse número;
+ * 
+ * @author Vitória Silva
+ * @version 1.0
+ * @since 29/03/2025
+ */
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===GERADOR DE TABUADA===");
+        System.out.println("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        //SOMA
+        System.out.println("ADIÇÃO");
+        for (int i = 1; i<=10; i++) {
+            System.out.println(numero + " + " + i + " = " + (numero + i));
+        }
+
+        //MULTIPLICAÇÃO
+        System.out.println("MULTIPLICAÇÃO");
+        for (int i = 1; i<=10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+
+        scanner.close();
     }
 }
